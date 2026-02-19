95c563e249da1f425c3708e11b09f73d4e8bfa15
jess-34 <jessdelvalle364@gmail.com>
2026-02-18 22:24:47 -0600

    Update Read.php

--- CRUD/Read.php
+++ CRUD/Read.php
@@ -5,7 +5,15 @@ $sql = "SELECT * FROM personas";
 $resultado = mysqli_query($conexion, $sql);
 ?>
 
-
+<table border="1">
+<tr>
+<th>ID</th>
+<th>Nombres</th>
+<th>Apellidos</th>
+<th>Edad</th>
+<th>Correo</th>
+<th>Acciones</th>
+</tr>
 
 
 
